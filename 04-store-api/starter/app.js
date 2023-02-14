8200692ed7207612ceb5caec39b7c1954be4c2a8
Mohamed Trachi <mohamedtrc3@gmail.com>
2023-02-14 23:12:33 +0300

    store api project

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,24 @@
-console.log('04 Store API')
+require("dotenv").config();
+require("express-async-errors");
+const express = require("express");
+const connectDB = require("./db/connect");
+const app = express();
+const notFound = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+const router = require("./routes/products");
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("<h1>store api</h1><a href='/api/v1/products'>products route</a>");
+});
+app.use("/api/v1/products", router);
+app.use(notFound);
+const port = process.env.PORT || 3000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`listening on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
